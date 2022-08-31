06abfda3290eed0de9dfc361659af5fdf1642dd1
Andrey Babaev <andrey.babaev@roonyx.tech>
2022-08-31 23:06:36 +0300

    ohl-9: added dataloader and augmenter

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="src",
+    version="1.0",
+    description="",
+    author="Roonyx",
+    author_email="roonyx.tech",
+    url="roonyx.tech",
+    packages=["src"],
+)
