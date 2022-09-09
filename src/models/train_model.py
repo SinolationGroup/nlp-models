9e001e05f4c666ea9c7d6c354f844597b1f6b8e3
Andrey Babaev <andrey.babaev@roonyx.tech>
2022-09-09 11:31:52 +0300

    ohl-13: updated readme and demo

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -59,7 +59,7 @@ def train():
         max_epochs=N_EPOCHS,
         log_every_n_steps=6,
         deterministic=True,
-        logger=wandb_logger,
+        # logger=wandb_logger,
         # accumulate_grad_batches=2,
         # val_check_interval=1000,
     )
